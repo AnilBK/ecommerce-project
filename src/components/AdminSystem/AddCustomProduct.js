2766500ff685279101bd98a0a001b14506ede368
Anilforextra <anilforextra@gmail.com>
2025-01-17 19:32:59 +0545

    AdminSystem: Add 'Add Products' button and image file picker.

--- src/components/AdminSystem/AddCustomProduct.js
+++ src/components/AdminSystem/AddCustomProduct.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import "./AddCustomProduct.css";
 
 function AddCustomProduct() {
+    const [userImage, setUserImage] = useState(null);
+    const [uploadedFilePath, setUploadedFilePath] = useState(null);
+
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => setUserImage(reader.result);
+            setUploadedFilePath(file.name);
+            reader.readAsDataURL(file);
+        }
+    };
+
+    function addProduct() {
+
+    }
+
     return (
         <div>
             <div>
@@ -16,6 +35,17 @@ function AddCustomProduct() {
                 <label for='has_colors_available'>Has Colors Available</label>
                 <input type='checkbox' name='has_colors_available'></input>
             </div>
+            <div>
+                <label for='custom_image'>Custom Image</label>
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageUpload}
+                    name='custom_image'
+                />
+            </div>
+            <br></br>
+            <button className="add-product-btn" onClick={addProduct}>Add Product</button>
         </div>
     );
 }
