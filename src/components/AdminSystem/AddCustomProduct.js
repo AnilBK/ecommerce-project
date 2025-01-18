3b5631eafc6d3cd11b387ca12296c36eec5af00a
Anilforextra <anilforextra@gmail.com>
2025-01-18 09:07:00 +0545

    AdminSystem: Add ability to upload list of screenshots & display the screenshots.

--- src/components/AdminSystem/AddCustomProduct.js
+++ src/components/AdminSystem/AddCustomProduct.js
@@ -5,6 +5,7 @@ import "./AddCustomProduct.css";
 function AddCustomProduct() {
     const [userImage, setUserImage] = useState(null);
     const [uploadedFilePath, setUploadedFilePath] = useState(null);
+    const [imageList, setImageList] = useState([]);
 
     const handleImageUpload = (event) => {
         const file = event.target.files[0];
@@ -16,36 +17,68 @@ function AddCustomProduct() {
         }
     };
 
-    function addProduct() {
+    const addImage = () => {
+        if (userImage && uploadedFilePath) {
+            setImageList((prevList) => [
+                ...prevList,
+                { name: uploadedFilePath, src: userImage },
+            ]);
 
-    }
+            // Reset the image input
+            setUserImage(null);
+            setUploadedFilePath(null);
+        } else {
+            alert("Please select an image before adding.");
+        }
+    };
+
+    const addProduct = () => {
+        console.log("Product Added");
+        console.log("Images: ", imageList);
+    };
 
     return (
         <div>
             <div>
-                <label for='product_name'>Product Name: </label>
+                <label htmlFor='product_name'>Product Name: </label>
                 <input type='text' placeholder='Enter product name' name='product_name'></input>
             </div>
             <div>
-                <label for='product_price'>Product Price: </label>
+                <label htmlFor='product_price'>Product Price: </label>
                 <input type='number' placeholder='Enter product price' name='product_price'></input>
             </div>
-            <br></br>
+            <br />
             <div>
-                <label for='has_colors_available'>Has Colors Available</label>
-                <input type='checkbox' name='has_colors_available'></input>
-            </div>
-            <div>
-                <label for='custom_image'>Custom Image</label>
+                <label htmlFor='custom_image'>Custom Image</label>
                 <input
                     type="file"
                     accept="image/*"
                     onChange={handleImageUpload}
                     name='custom_image'
                 />
+                <button onClick={addImage} className="add-image-btn">
+                    Add Image
+                </button>
             </div>
-            <br></br>
-            <button className="add-product-btn" onClick={addProduct}>Add Product</button>
+            <br />
+            <div>
+                <h4>Uploaded Images:</h4>
+                <ul>
+                    {imageList.map((image, index) => (
+                        <li key={index} style={{ listStyle: "none" }}>
+                            {image.name}
+                            <img
+                                src={image.src}
+                                alt={image.name}
+                                style={{ width: "100px", marginLeft: "10px" }}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <button className="add-product-btn" onClick={addProduct}>
+                Add Product
+            </button>
         </div>
     );
 }
